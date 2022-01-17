a93bbd16e507b6f21ee4abc4089d20c1d7721560
dlochmelis33 <dlochmelis@gmail.com>
2022-01-17 23:56:56 +0300

    Implement NioCSVReader

---
+++ utilities/src/main/java/ru/study21/jcsv/xxl/io/NioCSVReader.java
@@ -0,0 +1,164 @@
+package ru.study21.jcsv.xxl.io;
+
+import ru.study21.jcsv.xxl.common.BrokenContentsException;
+import ru.study21.jcsv.xxl.common.CSVMeta;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
+
+// for now only supports single-byte encoded charsets
+public class NioCSVReader implements CSVReader {
+
+    private final ByteBuffer buffer;
+    private final SeekableByteChannel inputChannel;
+    private final char separator;
+    private final CSVMeta meta;
+
+    public NioCSVReader(
+            SeekableByteChannel inputChannel,
+            int cacheSize,
+            boolean withHeader,
+            char separator
+    ) throws BrokenContentsException, IOException {
+        buffer = ByteBuffer.allocate(cacheSize);
+        this.inputChannel = inputChannel;
+        this.separator = separator;
+
+        if (withHeader) {
+            meta = CSVMeta.withNames(nextRow());
+        } else {
+            long pos = inputChannel.position();
+            meta = CSVMeta.withoutNames(nextRow().size());
+            inputChannel.position(pos);
+            buffer.clear();
+            buffer.flip(); // lim = pos = 0
+            rem = 0;
+        }
+    }
+
+    public NioCSVReader(
+            SeekableByteChannel inputChannel,
+            int cacheSize,
+            boolean withHeader
+    ) throws BrokenContentsException, IOException {
+        this(inputChannel, cacheSize, withHeader, ',');
+    }
+
+    public NioCSVReader(
+            SeekableByteChannel inputChannel,
+            int cacheSize,
+            char separator
+    ) throws BrokenContentsException, IOException {
+        this(inputChannel, cacheSize, false, separator);
+    }
+
+    public NioCSVReader(
+            SeekableByteChannel inputChannel,
+            int cacheSize
+    ) throws BrokenContentsException, IOException {
+        this(inputChannel, cacheSize, false, ',');
+    }
+
+    @Override
+    public CSVMeta meta() {
+        if (meta == null) {
+            throw new IllegalStateException("meta unknown, no reads yet");
+        }
+        return meta;
+    }
+
+    @Override
+    public List<String> nextRow() throws BrokenContentsException {
+        if (eof) {
+            return List.of();
+        }
+
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        boolean escaping = false;
+        boolean lastSymbolQuote = false;
+        boolean lastSymbolSeparator = false;
+
+        try {
+            while (true) {
+                byte c = readChar();
+                if (eof) {
+                    if (escaping) {
+                        throw new BrokenContentsException("EOF before escaped block was terminated");
+                    }
+                    if (sb.length() != 0) {
+                        result.add(sb.toString());
+                    }
+                    break;
+                }
+                if (c == '"') {
+                    lastSymbolSeparator = false;
+                    if (lastSymbolQuote) {
+                        sb.append('"');
+                    } else {
+                        lastSymbolQuote = true;
+                    }
+                    continue;
+                }
+                if (lastSymbolQuote) {
+                    escaping = !escaping;
+                }
+                lastSymbolQuote = false;
+                if (c == '\n') {
+                    if (lastSymbolSeparator) {
+                        throw new BrokenContentsException("last entry in row should not be followed by a separator");
+                    }
+//                    lastSymbolSeparator = false; // definitely false here
+                    if (escaping) {
+                        sb.append('\n');
+                    } else {
+                        result.add(sb.toString());
+                        sb.delete(0, sb.length());
+                        break;
+                    }
+                    continue;
+                }
+                if (c == separator) {
+                    if (escaping) {
+                        sb.append(separator);
+                    } else {
+                        result.add(sb.toString());
+                        sb.delete(0, sb.length());
+                        lastSymbolSeparator = true;
+                        continue;
+                    }
+                }
+                lastSymbolSeparator = false;
+                sb.append((char) c);
+            }
+            return result;
+
+        } catch (IOException e) {
+            BrokenContentsException ex = new BrokenContentsException("IO exception happened");
+            ex.addSuppressed(e);
+            throw ex;
+        }
+    }
+
+    private int rem = 0;
+    private boolean eof = false;
+
+    private byte readChar() throws IOException {
+        if (rem == 0) {
+            buffer.clear();
+            rem = inputChannel.read(buffer);
+            buffer.flip();
+        }
+        if (rem == -1) {
+            eof = true;
+            return 0;
+        }
+        byte c = buffer.get();
+        rem--;
+        return c;
+    }
+
+}
---
+++ utilities/src/test/java/ru/study21/jcsv/xxl/io/NioCSVReaderTest.java
@@ -0,0 +1,130 @@
+package ru.study21.jcsv.xxl.io;
+
+import org.junit.jupiter.api.Test;
+import ru.study21.jcsv.xxl.common.BrokenContentsException;
+import ru.study21.jcsv.xxl.common.CSVMeta;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class NioCSVReaderTest {
+
+    public final FileManager fm = FileManager.createTempDirectory("NioCSVReaderTest");
+    public Path file = fm.createTempFile("file");
+
+    public NioCSVReaderTest() throws IOException {
+    }
+
+    public void writeFile(String text) throws IOException {
+        Files.writeString(file, text, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    public NioCSVReader getReader(int cacheSize) throws IOException, BrokenContentsException {
+        return new NioCSVReader(Files.newByteChannel(file, StandardOpenOption.READ), cacheSize);
+    }
+
+    public NioCSVReader getReaderWithHeader(int cacheSize) throws IOException, BrokenContentsException {
+        return new NioCSVReader(Files.newByteChannel(file, StandardOpenOption.READ), cacheSize, true);
+    }
+
+    // tests based on RFC4180
+
+    @Test
+    public void test1_basic() throws IOException, BrokenContentsException {
+        writeFile("""
+                aaa,bbb,ccc
+                zzz,yyy,xxx
+                """);
+        NioCSVReader reader = getReader(4);
+        assertEquals(List.of("aaa", "bbb", "ccc"), reader.nextRow());
+        assertEquals(List.of("zzz", "yyy", "xxx"), reader.nextRow());
+        assertEquals(List.of(), reader.nextRow());
+        assertEquals(CSVMeta.withoutNames(3), reader.meta());
+    }
+
+    @Test
+    public void test2_eof() throws IOException, BrokenContentsException {
+        writeFile("""
+                aaa,bbb,ccc
+                zzz,yyy,xxx""");
+        NioCSVReader reader = getReader(5);
+        assertEquals(List.of("aaa", "bbb", "ccc"), reader.nextRow());
+        assertEquals(List.of("zzz", "yyy", "xxx"), reader.nextRow());
+        assertEquals(List.of(), reader.nextRow());
+        assertEquals(CSVMeta.withoutNames(3), reader.meta());
+    }
+
+    @Test
+    public void test3_names() throws IOException, BrokenContentsException {
+        writeFile("""
+                field_name,field_name,field_name
+                aaa,bbb,ccc
+                zzz,yyy,xxx""");
+        NioCSVReader reader = getReaderWithHeader(6);
+        assertEquals(List.of("aaa", "bbb", "ccc"), reader.nextRow());
+        assertEquals(List.of("zzz", "yyy", "xxx"), reader.nextRow());
+        assertEquals(List.of(), reader.nextRow());
+        assertEquals(CSVMeta.withNames(List.of("field_name", "field_name", "field_name")), reader.meta());
+    }
+
+    @Test
+    public void test4_wrong() throws IOException, BrokenContentsException {
+        // wrong column count
+        writeFile("""
+                aaa,bbb,ccc
+                zzz,yyy
+                """);
+        NioCSVReader reader = getReader(7);
+        assertEquals(List.of("aaa", "bbb", "ccc"), reader.nextRow());
+        assertThrows(BrokenContentsException.class, reader::nextRow);
+
+        // comma at line end
+        writeFile("""
+                aaa,bbb,ccc
+                xxx,yyy,zzz,
+                111,222,333""");
+        reader = getReader(7);
+        assertEquals(List.of("aaa", "bbb", "ccc"), reader.nextRow());
+        assertThrows(BrokenContentsException.class, reader::nextRow);
+    }
+
+    @Test
+    public void test5_quotes() throws IOException, BrokenContentsException {
+        writeFile("""
+                "aaa","bbb","ccc"
+                zzz,yyy,xxx""");
+        NioCSVReader reader = getReader(8);
+        assertEquals(List.of("aaa", "bbb", "ccc"), reader.nextRow());
+        assertEquals(List.of("zzz", "yyy", "xxx"), reader.nextRow());
+        assertEquals(List.of(), reader.nextRow());
+    }
+
+    @Test
+    public void test6_escapeBlock() throws IOException, BrokenContentsException {
+        writeFile("""
+                "aaa","b
+                bb","ccc"
+                zzz,yyy,xxx""");
+        NioCSVReader reader = getReader(9);
+        assertEquals(List.of("aaa", "bbb", "ccc"), reader.nextRow());
+        assertEquals(List.of("zzz", "yyy", "xxx"), reader.nextRow());
+        assertEquals(List.of(), reader.nextRow());
+    }
+
+    @Test
+    public void test7_escapeQuote() throws IOException, BrokenContentsException {
+        writeFile("""
+                "aaa","b""bb","ccc"
+                """);
+        NioCSVReader reader = getReader(10);
+        assertEquals(List.of("aaa", "b\"bb", "ccc"), reader.nextRow());
+        assertEquals(List.of(), reader.nextRow());
+    }
+
+}
